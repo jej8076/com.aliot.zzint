9ebe2eea6238f6e18cd2f3f0275065f7e942f83b
EJ <jej8076@gmail.com>
2022-06-24 10:47:16 +0900

    first commit

---
+++ src/main/kotlin/com/aliot/zzint/ZzintApplication.kt
@@ -0,0 +1,11 @@
+package com.aliot.zzint
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ZzintApplication
+
+fun main(args: Array<String>) {
+    runApplication<ZzintApplication>(*args)
+}
---
+++ src/test/kotlin/com/aliot/zzint/ZzintApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.aliot.zzint
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ZzintApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
